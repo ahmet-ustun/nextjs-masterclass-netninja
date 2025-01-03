15d8c87071f6a74e2100a5b93d33331b08fca409
Ahmet Ustun <austu@softserveinc.com>
2025-01-03 18:06:35 +0200

    Include not-found logic for all app and tickets

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,3 +1,7 @@
+import {notFound} from 'next/navigation'
+
+export const dynamicParams = true;
+
 export async function generateStaticParams() {
   const response = await fetch("http://localhost:4000/tickets");
   const tickets = await response.json();
@@ -13,6 +17,10 @@ async function getTicketDetails(id) {
       revalidate: 60,
     },
   });
+
+  if (!response.ok) {
+    notFound()
+  }
 
   return response.json();
 }
