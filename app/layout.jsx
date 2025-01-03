be664605cb0001760aa88de791002ac17e0368c0
Ahmet Ustun <austu@softserveinc.com>
2025-01-03 15:41:46 +0200

    Change the global font to MedievalSharp

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { MedievalSharp } from "next/font/google";
 
 // Components
 import Navbar from "./components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const medievalSharp = MedievalSharp({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
   title: "Create Next App",
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={medievalSharp.className}>
         <Navbar />
         {children}
       </body>
